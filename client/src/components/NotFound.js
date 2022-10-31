4bae05dea6e9b0e0dc1450bf526b89403235a317
Scott Williams <scottbwilliams.20@gmail.com>
2022-10-31 17:30:35 -0400

    error page complete

---
+++ client/src/components/NotFound.js
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
+
+function NotFound() {
+  return (
+    <main>
+      <div className="fourohfour-page">
+        <div className="fourohfour-title">
+          <h1>Oh No!</h1>
+        </div>
+        <div className="fourohfour-message">
+          <p>Looks like Luxio is lost.</p>
+          <Link className="body-link" to="/"><p>Go Home{`>`}</p></Link>
+        </div>
+        <div className="fourohfour-img">
+          <img alt="Luxio Pokémon Drawing" 
+          title="Luxio Pokémon Drawing" 
+          src="https://www.pngmart.com/files/22/Luxio-Pokemon-Transparent-PNG.png"
+          width="400"
+          height="400"></img>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default NotFound;
