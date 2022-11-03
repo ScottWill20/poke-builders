9953ba0a9388bfbdb958b2a53c17edd980bc517d
Scott Williams <scottbwilliams.20@gmail.com>
2022-11-03 11:29:53 -0400

    increased timeout to 1hr, fixed database init data, fixed 404 page

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -9,13 +9,13 @@ function NotFound() {
           <h1>Oh No!</h1>
         </div>
         <div className="fourohfour-message">
-          <p>Looks like Luxio is lost.</p>
+          <p>Looks like Shinx is lost.</p>
           <Link className="body-link" to="/"><p>Go Home{`>`}</p></Link>
         </div>
         <div className="fourohfour-img">
           <img alt="Luxio Pokémon Drawing" 
           title="Luxio Pokémon Drawing" 
-          src="https://www.pngmart.com/files/22/Luxio-Pokemon-Transparent-PNG.png"
+          src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/403.png"
           width="400"
           height="400"></img>
         </div>
--- server/src/main/java/learn/pokemon/security/JwtConverter.java
+++ server/src/main/java/learn/pokemon/security/JwtConverter.java
@@ -18,7 +18,7 @@ public class JwtConverter {
 
     private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
     private final String ISSUER = "poke-builder";
-    private final int EXPIRATION_MINUTES = 15;
+    private final int EXPIRATION_MINUTES = 60;
     private final int EXPIRATION_MILLIS = EXPIRATION_MINUTES * 60 * 1000;
 
     // Take an instance of `AppUser` as a parameter, instead of `UserDetails`
